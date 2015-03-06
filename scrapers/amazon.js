447ae892d2c7ee0d54e493a39900e9fab4eec2b8
Nick Carneiro <nickc@indeed.com>
2015-03-05 21:41:03 -0800

    amazon scraper passes test

--- scrapers/amazon.js
+++ scrapers/amazon.js
@@ -5,7 +5,28 @@ var cheerio = require('cheerio');
  * @param {string} html extract
  */
 var extractProperties = function(html) {
-    return {};
+    var $ = cheerio.load(html);
+    var title = $('#productTitle').text();
+    var author = $('.contributorNameID').text();
+    var description = $($('noscript')[1]).html().trim();
+    console.log(description);
+    var pagesText = $('.content li:contains("pages")').text();
+    var pagesCountGroups = pagesText.match(/([\d]+)/);
+    var pages = parseInt(pagesCountGroups[0]);
+    var isbnText = $('.content li:contains("ISBN-10:")').text();
+    var isbn10 = isbnText.replace('ISBN-10:', '').trim();
+    //todo: validate isbn
+    var isbn13Text = $('.content li:contains("ISBN-13")').text();
+    var isbn13 = isbn13Text.replace('ISBN-13:', '').trim();
+
+    return {
+        title: title,
+        author: author,
+        description: description,
+        pages: pages,
+        isbn10: isbn10,
+        isbn13 : isbn13
+    };
 };
 
 module.exports = {
--- test/test.js
+++ test/test.js
@@ -1,16 +1,19 @@
 var test = require('tape');
 var fs = require('fs');
 var amazonScraper = require('../scrapers/amazon');
+
+
 test('test amazon html extraction', function (t) {
-    var amazonHtml = fs.readFileSync(__dirname + '/tsukuru.html');
+    var amazonHtml = fs.readFileSync(__dirname + '/tsukuru_amazon.html');
     var bookProperties = amazonScraper.extractProperties(amazonHtml);
     var expectedProperties = {
+        title: 'Colorless Tsukuru Tazaki and His Years of Pilgrimage: A novel',
         isbn10: '0385352107',
         isbn13: '978-0385352109',
         pages: 400,
         author: 'Haruki Murakami',
-        description: fs.readFileSync(__dirname + '/tsukuru_properties.txt', 'utf-8')
+        description: fs.readFileSync(__dirname + '/tsukuru_description.txt', 'utf-8')
     };
-    t.equals(bookProperties, expectedProperties);
+    t.deepEquals(bookProperties, expectedProperties);
     t.end();
 });
