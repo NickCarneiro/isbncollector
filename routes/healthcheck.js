13b78c7bb1c2c17ef4e26a727fbbff234e257889
Nick Carneiro <nickc@indeed.com>
2015-03-17 09:19:11 -0700

    make healthcheck unhealthy if no activity

--- routes/healthcheck.js
+++ routes/healthcheck.js
@@ -5,6 +5,13 @@ var storageUtils = require('../scrapers/storage_utils');
 router.get('/', function(req, res) {
     storageUtils.getHealthcheck(function(docs) {
         res.setHeader('Content-Type', 'application/json');
+        docs.forEach(function(doc) {
+            var now = Date.now();
+            if (now - doc.lastEvent > 1000*60) {
+                doc.healthy = false;
+                doc.message = 'No activity for 60 seconds.'
+            }
+        });
         res.end(JSON.stringify(docs));
     });
 
