966f24944ca7b4e724c706d8771eb21037d27d12
Nick Carneiro <nickc@indeed.com>
2015-03-25 22:19:51 -0700

    show list of search results when searching for title, closes #12

--- routes/book.js
+++ routes/book.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res) {
     }
     var query = {};
     isbn = isbn.replace('-', '');
-    if (isbn.match(/^[\d\w]+$/)) {
+    if (isbn.match(/^[\dX]+$/)) {
         if (isbn.length > 10) {
             query['isbn13'] = isbn;
         } else {
@@ -48,39 +48,30 @@ var handleRequest = function(query, db, res, req) {
     var collection = db.collection('books');
     if (query['$text']) {
         var options = {score: {$meta: "textScore"}};
-        collection.find(query, options).sort({score: {$meta: 'textScore'}}).toArray(handleResults.bind(this, db, query, res, req));
+        collection.find(query, options).sort({score: {$meta: 'textScore'}}).toArray(handleSearchResults.bind(this, db, query, res, req));
     } else {
         collection.find(query).toArray(handleResults.bind(this, db, query, res, req));
     }
 
 };
 
+
+var handleSearchResults = function(db, query, res, req, err, docs) {
+    var keyword = query['$text']['$search'];
+
+    res.render('browse', {books: docs, keyword: keyword, path: req.baseUrl});
+};
+
+
 var handleResults = function(db, query, res, req, err, docs) {
-    var keyword = query.isbn13 || query.isbn10 || query['$text']['$search'];
+    var keyword = query.isbn13 || query.isbn10;
     if (err === null && docs && docs.length > 0) {
         console.log('found book in the mongo');
             res.render('book', {properties: docs[0], keyword: keyword, path: req.baseUrl});
     } else {
-        // if the book isn't in the mongo, fetch it from another source.
-        amazon.searchForBook(keyword, function(bookProperties, requestFailed) {
-            if (bookProperties && !requestFailed) {
-                // save this new book to the mongo
-                console.log('found book externally.');
-                var collection = db.collection('books');
-                collection.insert(bookProperties, {w: 1}, function(err, records) {
-                    if (!err) {
-                        console.log('saving to mongo.');
-                    } else {
-                        console.log(err);
-                    }
-                });
-                res.render('book', {properties: bookProperties, keyword: keyword, path: req.baseUrl});
-            } else {
-                res.status(404);
-                var errorMessage = bookProperties || 'No book found';
-                res.render('bookError', {errorMessage: errorMessage});
-            }
-        });
+        res.status(404);
+        var errorMessage = 'No book found';
+        res.render('bookError', {errorMessage: errorMessage});
     }
 
 };
