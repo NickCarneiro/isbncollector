90c265cd19973fb50900802284aab39411882020
Nick Carneiro <nickc@indeed.com>
2015-03-26 23:51:51 -0700

    improve api, added docs

--- routes/api.js
+++ routes/api.js
@@ -3,21 +3,25 @@ var config = require('../config');
 var express = require('express');
 var router = express.Router();
 var db;
-router.get('/v1', function(req, res) {
+
+router.get('/', function(req, res) {
+
+    res.render('api', {path: req.baseUrl, keyword: ''});
+});
+
+router.get('/v1/books/:isbn', function(req, res) {
 
     var query = {};
-    var isbn = req.query.isbn;
+    var isbn = req.params.isbn;
     if (isbn && isbn.length > 10) {
         query['isbn13'] = isbn;
     } else if (isbn) {
         query['isbn10'] = isbn;
     }
-    if (req.query.title) {
-        query['title'] = req.query.title;
-    }
-    if (!req.query.title && !req.query.isbn) {
+    if (!isbn) {
         res.status(400);
-        res.end(JSON.stringify({ error: 'no isbn or title params'}));
+        res.json({ error: 'no isbn param'});
+        return;
     }
     if (db) {
         getBookFromMongo(db, query, res);
@@ -32,16 +36,52 @@ router.get('/v1', function(req, res) {
 
 var getBookFromMongo = function(db, query, res) {
     var collection = db.collection('books');
-    collection.find(query).toArray(function(err, docs) {
-        if (err === null && docs && docs.length > 0) {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(docs));
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(404);
-            res.end(JSON.stringify({error: 'no job found for given params'}));
-        }
-    });
+    if (query['$text']) {
+        var options = {score: {$meta: "textScore"}};
+        collection.find(query, options).sort({score: {$meta: 'textScore'}}).toArray(function(err, docs) {
+            if (err === null && docs && docs.length > 0) {
+                res.setHeader('Content-Type', 'application/json');
+                res.json(docs);
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(404);
+                res.json({error: 'no job found for given title'});
+            }
+        });
+    } else {
+
+        collection.find(query).toArray(function (err, docs) {
+            if (err === null && docs && docs.length > 0) {
+                res.setHeader('Content-Type', 'application/json');
+                res.json(docs);
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(404);
+                res.json({error: 'no job found for given isbn'});
+            }
+        });
+    }
 };
+
+
+router.get('/v1/search', function(req, res) {
+
+    var title = req.query.title;
+    if (!title) {
+        res.status(400);
+        res.json({ error: 'no title param'});
+        return;
+    }
+    var query = {$text: {$search: title}};
+
+    if (db) {
+        getBookFromMongo(db, query, res);
+    } else {
+        MongoClient.connect(config.MONGO_URL, function (err, connection) {
+            db = connection;
+            getBookFromMongo(db, query, res);
+        });
+    }
+});
 
 module.exports = router;
