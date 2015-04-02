6605fc89ca23d62e9453eed3ccd3b4c8dd9908e2
Nick Carneiro <nickc@indeed.com>
2015-04-02 09:45:41 -0700

    use numeral to format total book count on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+var numeral = require('numeral');
 
 var url = 'mongodb://localhost:27017/isbncollector';
 /* GET home page. */
@@ -8,10 +9,11 @@ router.get('/', function(req, res, next) {
   MongoClient.connect(url, function(err, db) {
     var collection = db.collection('books');
     collection.find({}).count(function(err, count) {
+      var formattedCount = numeral(count).format('0,0');
       if (err) {
         res.render('error', {message: 'Error connecting to mongo'});
       } else {
-        res.render('index', {totalBookCount: count, path: req.baseUrl, keyword: ''});
+        res.render('index', {totalBookCount: formattedCount, path: req.baseUrl, keyword: ''});
       }
       db.close();
     });
